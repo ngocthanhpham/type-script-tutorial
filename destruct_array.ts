7532e66dfa13cae613531195fe94f41aea40a925
taphets <ngocthanhpham8693@gmail.com>
2017-03-31 14:08:19 +0700

    update code

--- destruct_array.ts
+++ destruct_array.ts
@@ -3,4 +3,4 @@ let [emp1, emp2, emp3] = employees;
 
 console.log(emp1);
 console.log(emp2);
-console.log(emp3);``
+console.log(emp3);
--- dist/destruct_array.js
+++ dist/destruct_array.js
@@ -3,5 +3,4 @@ var emp1 = employees[0], emp2 = employees[1], emp3 = employees[2];
 console.log(emp1);
 console.log(emp2);
 console.log(emp3);
-"";
 //# sourceMappingURL=destruct_array.js.map
